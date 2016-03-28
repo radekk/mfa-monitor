9df1b31373dd7d75718176c92c8cdd2d387937ea
radekk <radekk@protonmail.com>
2016-03-28 14:44:46 +0200

    Configuration script for cron settings.
    - npm run configure:cron
    - file: gulp/tasks/configure.cron.js

--- gulp/tasks/configure.cron.js
+++ gulp/tasks/configure.cron.js
@@ -1,6 +1,28 @@
 'use strict';
 
 const gulp = require('gulp');
+const helper = require('gulp/helper');
 
-gulp.task('configure:cron', () => {
+gulp.task('configure:cron', (cb) => {
+  const isNotEmpty = helper.validators.isNotEmpty;
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Set name for your cron task:',
+      default: 'scheduler',
+      validate: isNotEmpty
+    },
+    {
+      type: 'input',
+      name: 'expression',
+      message: 'Set schedule:',
+      default: '* * * * *',
+      validate: isNotEmpty
+    }
+  ];
+
+  helper.prompt(questions).then(input => {
+    helper.updateConfig('cron', input).then(cb).catch(cb);
+  });
 });
