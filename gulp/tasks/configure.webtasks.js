7834922aa0f3133aea1b36c55564fa393768695b
radekk <radekk@protonmail.com>
2016-04-01 19:52:20 +0200

    Update configuration tasks.

--- gulp/tasks/configure.webtasks.js
+++ gulp/tasks/configure.webtasks.js
@@ -13,32 +13,35 @@ gulp.task('configure:webtasks', cb => {
 
   let config = [];
   const monitors = webtasks.filter(wt => wt.type === 'monitor').map(wt => wt.name);
-  const notifier = webtasks.filter(wt => wt.type === 'notifier').map(wt => wt.name);
+  const notifiers = webtasks.filter(wt => wt.type === 'notifier').map(wt => wt.name);
   const questions = [
     {
-      type: 'list',
-      name: 'notifier',
+      type: 'checkbox',
+      name: 'notifiers',
       message: 'Where do you want to send your alerts',
-      choices: notifier
+      choices: notifiers,
+      validate: helper.validators.atLeastOneSelected
     },
     {
       type: 'checkbox',
       name: 'monitors',
       message: 'Which services do you want to monitor',
-      choices: monitors
+      choices: monitors,
+      validate: helper.validators.atLeastOneSelected
     }
   ];
 
   helper.prompt(questions).then(input => {
-    assert(input.notifier, 'Invalid settings for notifier');
+    assert(input.notifiers, 'Invalid settings for notifiers');
     assert(input.monitors, 'Invalid settings for monitors');
 
-    let notifier = webtasks.filter(wt => wt.name === input.notifier).pop();
-    let monitors = _.flatten(input.monitors.map(
-      monitor => webtasks.filter(wt => wt.name === monitor)
-    ));
+    let notifiers = _.flatten(input.notifiers.map(
+      notifier => webtasks.filter(wt => wt.name === notifier)));
 
-    return Promise.resolve(_.flatten([notifier, monitors]));
+    let monitors = _.flatten(input.monitors.map(
+      monitor => webtasks.filter(wt => wt.name === monitor)));
+
+    return Promise.resolve(_.flatten([notifiers, monitors]));
   }).then(data => {
     return Promise.each(data, wt =>
       helper.promptWebtaskSettings(wt).then(input => {
