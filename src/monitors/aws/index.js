17a4262bc6f592b2ebf81e6975770d1650d8a564
radekk <radekk@protonmail.com>
2016-06-09 12:23:00 +0200

    New monitor - AWS.
    It uses two different calls to on API to get information about users
    with disabled MFA.
    
    1. listUsers
    2. listMFADevices

---
+++ src/monitors/aws/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const assert = require('assert');
+const AWS = require('aws-sdk');
+const maxItems = 1000;
+const Promise = require('bluebird').Promise;
+
+/**
+ * @param {secret} ACCESS_KEY_ID     - Amazon access key id
+ * @param {secret} SECRET_ACCESS_KEY - Amazon secret access key
+ * @return JSON ['john', 'mark']
+ */
+module.exports = (ctx, cb) => {
+  assert(ctx.secrets, 'Secrets not set!');
+  assert(ctx.secrets.ACCESS_KEY_ID, 'ACCESS_KEY_ID not set!');
+  assert(ctx.secrets.SECRET_ACCESS_KEY, 'SECRET_ACCESS_KEY not set!');
+
+  AWS.config.update({
+    accessKeyId: ctx.secrets.ACCESS_KEY_ID,
+    secretAccessKey: ctx.secrets.SECRET_ACCESS_KEY
+  });
+
+  const iam = new AWS.IAM();
+  const getUserMFADevices = (userName =>
+    new Promise((resolve, reject) =>
+      iam.listMFADevices({MaxItems: maxItems, UserName: userName}, (err, devices) => {
+        if (err) return reject(err);
+        resolve({user: userName, isMFADevice: !!devices.MFADevices.length});
+      })
+  ));
+
+  new Promise((resolve, reject) =>
+    iam.listUsers({MaxItems: maxItems, PathPrefix: '/'}, (err, data) => {
+      if (err) return reject(err);
+      resolve(data.Users.filter(user => !!user['PasswordLastUsed']));
+    })
+  ).then(users =>
+    Promise.all(users.map(user => user.UserName).map(getUserMFADevices)).then(data =>
+      cb(null, data.filter(user => !user.isMFADevice).map(row => row.user)))
+  ).catch(err => cb(err));
+};
